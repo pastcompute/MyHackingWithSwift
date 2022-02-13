b45c44de2c1bf5aa7b4127fad2617921e933f943
Andrew McDonnell <bugs@andrewmcdonnell.net>
2022-02-13 23:11:46 +1030

    Blank project that builds on iPad3 / ios9.3

---
+++ BlankProject/BlankProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlankProject
+//
+//  Created by Andrew McDonnell on 13/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
