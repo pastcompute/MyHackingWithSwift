788c02e9c5b101042b1ac51bd3078a87470f8ecf
Andrew McDonnell <bugs@andrewmcdonnell.net>
2022-03-19 16:09:47 +1030

    Duplicate project 7 for project 9

---
+++ Project9/Project9/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Andrew McDonnell on 14/3/2022.
+//
+
+import Foundation
+
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
